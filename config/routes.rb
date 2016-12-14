2207419704f8e6d89e80346912cb53bb3a5a24a4
Swapnil Singh <swappp7singh@gmail.com>
2016-12-14 21:49:16 +0530

    root route, simple route, default rouote included. only using root and simple route!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+
+  # root route
+  # short form
+  # get 'demo#index'
+
+  match '/', :to  =>  'demo#index', :via  => :get
+
+  # Simple route
   get 'demo/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # default route, will go away in future
+  # match 'demo/index', :to => 'demo#index', :via =>  :get
+  # short form
+  # get ':controller(/:action/:id)'
 end
