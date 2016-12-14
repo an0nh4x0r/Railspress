2cf89a72033177f356e174f2d975c2a2a77831fa
Swapnil Singh <swappp7singh@gmail.com>
2016-12-14 22:32:39 +0530

    render method to render templates

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,6 +3,11 @@ class DemoController < ApplicationController
   layout(false)
 
   def index
+    render :template => 'demo/hello'
+  end
+
+  def hello
+    render :template => 'demo/index'
   end
 
 end
