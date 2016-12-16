bad4d0f41c84f968beedeff989dec73d2445f5df
Swapnil Singh <swappp7singh@gmail.com>
2016-12-16 09:38:03 +0530

    Modified controller

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,11 +3,11 @@ class DemoController < ApplicationController
   layout(false)
 
   def index
-    render :template => 'demo/hello'
+    render :template => 'demo/index'
   end
 
   def hello
-    render :template => 'demo/index'
+    render :template => 'demo/hello'
   end
 
   def other_hello
