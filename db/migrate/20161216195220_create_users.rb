1777afb8d497e39f7e67709401aff93c25300f26
Swapnil Singh <swappp7singh@gmail.com>
2016-12-17 20:03:55 +0530

    added the table schema in the migration of create_users

--- db/migrate/20161216195220_create_users.rb
+++ db/migrate/20161216195220_create_users.rb
@@ -1,8 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
+  def up
     create_table :users do |t|
-
+      t.column 'first_name', :string, :limit => 25
+      t.column 'last_name', :string, :limit => 50
+      t.column 'email', :string, :default => '', :null => false
+      t.column 'password', :string, :limit => 40
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :users
   end
 end
